39e2086ba52c6b4559ee0cf17878fde9eca44aaa
Jeff Gonis <jeff.gonis@gmail.com>
2013-08-06 17:49:54 -0600

    finished chapter 3 and working on chapter 4

--- lschemer/littleSchemerV2.scm
+++ lschemer/littleSchemerV2.scm
@@ -26,3 +26,98 @@
     (cond ((null? l) (quote ()))
           (else (cons (car (car l)) (firsts (cdr l)))))))
 
+(define insertR
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons old (cons new (cdr lat))))
+          (else (cons (car lat) (insertR new old (cdr lat)))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons new (cons old (cdr lat))))
+          (else (cons (car lat) (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons new (cdr lat)))
+          (else (cons (car lat) (subst new old (cdr lat)))))))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond ((null? lat) (quote ()))
+          ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+          (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) a) (multirember a (cdr lat)))
+          (else (cons (car lat) (multirember a (cdr lat)))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons old
+                                     (cons new (multiinsertR new
+                                                              old
+                                                              (cdr lat)))))
+          (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons new
+                                     (cons old (multiinsertL new
+                                                             old
+                                                             (cdr lat)))))
+          (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons new (multisubst new old (cdr lat))))
+          (else (cons (car lat) (multisubst new old (cdr lat)))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define j-+
+  (lambda (x y)
+    (cond ((zero? y) x)
+          (else (j-+ (add1 x) (sub1 y))))))
+
+(define j--
+  (lambda (x y)
+    (cond ((zero? y) x)
+          (else (j-- (sub1 x) (sub1 y))))))
+
+(define addtup
+  (lambda (tup)
+    (cond ((null? tup) 0)
+          (else (j-+ (car tup) (addtup (cdr tup)))))))
+
+(define j-*
+  (lambda (x y)
+    (cond ((zero? y) 0)
+          (else (j-+ x (j-* x (sub1 y)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond ((null? tup1) tup2)
+          ((null? tup2) tup1)
+          (else (cons (j-+ (car tup1) (car tup2))
+                      (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define j->
+  (lambda (x y)
+    (cond ((or (zero? x) (and (zero? y) (zero? x))) #f)
+          ((zero? y) #t)
+          (else (j-> (sub1 x) (sub1 y))))))
+
