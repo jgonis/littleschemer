d5291af92f1710b4c64dbf0a1859fd42e88e64eb
Jeff Gonis <jeff.gonis@gmail.com>
2013-09-13 13:16:04 -0600

    Chapter 7 work.

--- lschemer/littleSchemerV2.scm
+++ lschemer/littleSchemerV2.scm
@@ -332,3 +332,53 @@
           (else (edd (edd1 x) (zub1 y))))))
 
 ;;Chapter 7 Friends and Relations
+(define set?
+  (lambda (lat)
+    (cond ((null? lat) #t)
+          ((member? (car lat) (cdr lat)) #f)
+          (else (set? (cdr lat))))))
+
+(define makeset
+  (lambda (lat)
+    (cond ((null? lat) '())
+          ((member? (car lat) (cdr lat)) (makeset (cdr lat)))
+          (else (cons (car lat) (makeset (cdr lat)))))))
+
+(define alt-makeset
+  (lambda (lat)
+    (cond ((null? lat) '())
+          (else (cons (car lat)
+                      (alt-makeset (multirember (car lat) (cdr lat))))))))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond ((null? set1) #t)
+          ((member? (car set1) set2) (subset? (cdr set1) set2))
+          (else #f))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and (subset? set1 set2) (subset? set2 set1))))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond ((null? set1) #f)
+          ((member? (car set1) set2) #t)
+          (else (intersect? (cdr set1) set2)))))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond ((null? set1) '())
+          ((member? (car set1) set2) (cons (car set1)
+                                           (intersect (cdr set1) set2)))
+          (else (intersect (cdr set1))))))
+
+(define union
+  (lambda (set1 set2)
+    (cond ((null? set1) set2)
+          ((member? (car set1) set2) (union (cdr set1) set2))
+          (else (cons (car set1) (union (cdr set1) set2))))))
+
+(define intersectall
+  (lambda (l-set)
+    (quote ())))
